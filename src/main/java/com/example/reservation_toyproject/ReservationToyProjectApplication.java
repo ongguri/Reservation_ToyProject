1a5292bc2990a4a4803f494c7b5dcaa2d68b8859
ongguri <rkdtjsdyd321@naver.com>
2023-08-01 17:37:25 +0900

    first commit

---
+++ src/main/java/com/example/reservation_toyproject/ReservationToyProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.reservation_toyproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservationToyProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservationToyProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/reservation_toyproject/ReservationToyProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reservation_toyproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservationToyProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
