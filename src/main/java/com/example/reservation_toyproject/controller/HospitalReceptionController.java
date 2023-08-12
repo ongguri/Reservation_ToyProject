da8be52ac1fff07aa54ef5e8b067ed12ae893f55
ongguri <rkdtjsdyd321@naver.com>
2023-08-12 17:15:26 +0900

    병원 접수 확인-reception 이란 이름으로 전부 수정/분류 및 컨트롤러 생성

--- src/main/java/com/example/reservation_toyproject/controller/HospitalReceptionController.java
+++ src/main/java/com/example/reservation_toyproject/controller/HospitalReceptionController.java
@@ -1,19 +1,22 @@
 package com.example.reservation_toyproject.controller;
 
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
-@RequestMapping("/reservations")
+@RequestMapping("/receptions")
 @Controller
-public class UserReservationController {
+public class HospitalReceptionController {
+
 
     @GetMapping
     public String reservations(ModelMap map) {
-        map.addAttribute("reservations", List.of()); // 임시값
-        return "reservations/index";
+        map.addAttribute("receptions", "receptionsData"); // 임시값
+        return "receptions/index";
     }
 }
