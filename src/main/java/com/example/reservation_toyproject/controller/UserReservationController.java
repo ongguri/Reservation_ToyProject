53e83ada3469f5bf66e3234d754230ff4aae4cc4
ongguri <rkdtjsdyd321@naver.com>
2023-08-21 19:00:53 +0900

    UserReservation 목 데이터 화면에 뿌림

--- src/main/java/com/example/reservation_toyproject/controller/UserReservationController.java
+++ src/main/java/com/example/reservation_toyproject/controller/UserReservationController.java
@@ -1,19 +1,36 @@
 package com.example.reservation_toyproject.controller;
 
+import com.example.reservation_toyproject.domain.type.SearchType;
+import com.example.reservation_toyproject.response.UserReservationResponse;
+import com.example.reservation_toyproject.service.ReservationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
+import org.springframework.web.bind.annotation.RequestParam;
 
+@RequiredArgsConstructor
 @RequestMapping("/reservations")
 @Controller
 public class UserReservationController {
 
+    private final ReservationService reservationService;
+
     @GetMapping
-    public String reservations(ModelMap map) {
-        map.addAttribute("reservations", List.of()); // 임시값
+    public String reservations(
+        @RequestParam(required = false) SearchType searchType,
+        @RequestParam(required = false) String searchValue,
+        @PageableDefault(size = 15, sort = "createdAt", direction = Direction.DESC) Pageable pageable,
+        ModelMap map
+    ) {
+        map.addAttribute("reservations", reservationService.searchUserReservation(
+            searchType, searchValue, pageable).map(UserReservationResponse::from));
         return "reservations/index";
     }
 }
